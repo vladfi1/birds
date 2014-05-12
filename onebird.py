e753b5fe3e0ea310d0a1cce4132818bf69140896
vladfi1 <vladfi2@gmail.com>
2014-05-12 18:19:02 -0400

    Use scope_exclude.

--- onebird.py
+++ onebird.py
@@ -7,18 +7,19 @@ from utils import *
 # model
 width = 4
 height = 4
+cells = width * height
 num_features = 4
 total_birds = 1
 
 ripl.assume('width', width)
 ripl.assume('height', height)
-ripl.assume('cells', '(* width height)')
+ripl.assume('cells', cells)
 ripl.assume('dist_max2', '18')
 
 ripl.assume('cell2X', '(lambda (cell) (int_div cell height))')
 ripl.assume('cell2Y', '(lambda (cell) (int_mod cell height))')
 #ripl.assume('cell2P', '(lambda (cell) (make_pair (cell2X cell) (cell2Y cell)))')
-ripl.assume('XY2cell', '(lambda (x y) (+ (* height x) y))')
+ripl.assume('XY2cell', '(lambda (x y) (to_atom (+ (* height x) y)))')
 
 ripl.assume('square', '(lambda (x) (* x x))')
 
@@ -66,7 +67,7 @@ ripl.assume('fold', """
 """)
 
 # phi is the unnormalized probability of a bird moving
-# from cell i to cell j between on day d
+# from cell i to cell j on day d
 ripl.assume('phi', """
   (mem (lambda (y d i j)
     (if (> (cell_dist2 i j) dist_max2) 0
@@ -104,15 +105,15 @@ ripl.assume('get_bird_move_dist', """
 # samples where a bird would move to from cell i on day d
 ripl.assume('move', """
   (lambda (y d i)
-    (scope_include y d
-      (categorical (scope_include y -1 (get_bird_move_dist y d i)))
+    (scope_include (quote move) (array y d i)
+      (categorical (scope_exclude (quote move) (get_bird_move_dist y d i)))
     )
   )
 """)
 
 ripl.assume('get_bird_pos', """
   (mem (lambda (y d id)
-    (if (= d 1) 0
+    (if (= d 1) atom<0>
       (move y (- d 1) (get_bird_pos y (- d 1) id))
     )
   ))
@@ -131,7 +132,7 @@ ripl.assume('count_birds', """
   )
 """)
 
-ripl.assume('observe_birds', '(mem (lambda (y d i) (poisson (+ (count_birds y d i) (exp -100)))))')
+ripl.assume('observe_birds', '(mem (lambda (y d i) (poisson (+ (count_birds y d i) 0.0001))))')
 
 features_file = "release/onebird-features.csv"
 observations_file = "release/onebird-observations.csv"
@@ -152,13 +153,10 @@ def loadObservations(years):
     for (d, ns) in observations[y]:
       for i, n in enumerate(ns):
         print y, d, i
-        ripl.observe('(observe_birds %d %d %d)' % (y, d, i), n)
-      
-      if d > 0:
-        ripl.infer('(gibbs %d %d %d)' % (y, d-1, 1))
+        ripl.observe('(observe_birds %d %d atom<%d>)' % (y, d, i), n)
 
 def getBirds(y, d):
-  return [[ripl.sample('(count_birds %d %d (XY2cell %d %d))' % (y, d, x, y)) for y in range(height)] for x in range(width)]
+  return [ripl.sample('(count_birds %d %d atom<%d>)' % (y, d, i)) for i in range(cells)]
 
 def printYear(y):
   for d in range(1, 21):
@@ -167,7 +165,8 @@ def printYear(y):
 def getBird(y, id):
   return [ripl.sample('(get_bird_pos %d %d %d)' % (y, d, id)) for d in range(1, 21)]
 
-
+def inferMove(n):
+  ripl.infer('(gibbs move one %d)' % n)
 
 years = range(1, 2)
 
