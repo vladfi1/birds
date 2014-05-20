6e3570e880f8de7633fed633a976e2ad0bcc2237
Vlad Firoiu <vladfi2@gmail.com>
2014-05-20 00:58:10 -0400

    Debugging continuous model.

--- train_birds.py
+++ train_birds.py
@@ -21,7 +21,7 @@ D = 2
 runs = 1
 
 # these are estimates from onebird
-hypers = [1.011776136710042, 3.315465986337299, 2.6465177320342272, 3.6932748455768913]
+hypers = [1.7223437017981613, 3.4445635302098445, 2.5864910484370607, 3.5926413162397104]
 
 params = {
   "name":name,
@@ -36,50 +36,64 @@ params = {
 
 Continuous.loadAssumes(ripl, **params)
 
-observes = {}
-
+print "Predicting observes"
+observes = []
 for y in range(Y):
   for d in range(D):
     for i in range(cells):
-      observes[(y, d, i)] = ripl.predict('(observe_birds %d %d %d)' % (y, d, i))
-
-#true_bird_moves = ripl.predict('(get_birds_moving4)')
+      n = ripl.predict('(observe_birds %d %d %d)' % (y, d, i))
+      observes.append((y, d, i, n))
 
 def getBirdMoves():
+  print "Sampling bird movements"
+  
+  #return ripl.sample('(get_birds_moving4)')
   bird_moves = []
+  
   for y in range(Y):
     for d in range(D-1):
       for i in range(cells):
         for j in range(cells):
-          print y, d, i, j
-          bird_moves.append(_strip_types(ripl.sivm.core_sivm.engine.predict([s.symbol('get_birds_moving')] + map(s.number, [y, d, i, j]))[1]))
+          bird_moves.append(_strip_types(ripl.sivm.sample(['get_birds_moving'] + map(s.number, [y, d, i, j]))['value']))
   
   return bird_moves
 
 true_bird_moves = getBirdMoves()
 
+import multiprocessing
+p = multiprocessing.cpu_count() / 2
+
+print "Using %d particles" % p
+
 def sweep(r, *args):
+  #for y in range(Y):
+  #  r.infer("(pgibbs %d ordered %d 1)" % (y, p))
   for y in range(Y):
-    r.infer("(pgibbs %d ordered 2 1)" % y)
-  r.infer("(mh default one %d)" % (5 * cells))
+    #r.infer("(mh %d one %d)" % (y, 1))
+    r.infer("(mh default one %d)" % cells)
 
-ripl.clear()
-Continuous.loadAssumes(ripl, **params)
+def computeScore():
+  infer_bird_moves = getBirdMoves()
 
-for (y, d, i), n in observes.iteritems():
-  ripl.observe('(observe_birds %d %d %d)' % (y, d, i), n)
+  score = 0
 
-for i in range(Y * D):
-  print i
-  sweep(ripl)
+  for n1, n2 in zip(true_bird_moves, infer_bird_moves):
+    score += (n1 - n2) ** 2
 
-#infer_bird_moves = ripl.predict('(get_birds_moving4)')
+  return score
 
-infer_bird_moves = getBirdMoves()
+def run():
+  print "Starting run"
+  ripl.clear()
+  Continuous.loadAssumes(ripl, **params)
+  
+  print "Loading observations"
+  for (y, d, i, n) in observes:
+    ripl.observe('(observe_birds %d %d %d)' % (y, d, i), n)
 
-score = 0
+  for i in range(Y * D):
+    print "Inference step %d" % i
+    sweep(ripl)
+    print computeScore()
 
-for n1, n2 in zip(true_bird_moves, infer_bird_moves):
-  score += (n1 - n2) ** 2
-
-print score
+run()
